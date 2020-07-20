671b5ef83b06611421a2cd846a4b6690cbd000d8
DEVECLOVER <42308974+DEVECLOVER@users.noreply.github.com>
2020-07-20 22:08:37 +0800

    Add files via upload

---
+++ LinearList.SDk/ILinearList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearList.SDk
+{
+    public interface ILinearList<T>
+    {
+        void AddElement(T item);
+        void RemoveElement(T index);
+        void Insert(T item, int index);
+        void ChangeElement(T item, int index);
+        void Clear();
+        bool IsEmpty();
+        //T this[int index] { get; set; }
+        T GetElement(int index);
+        int Locate(T item);
+        int GetLength();
+    }
+}
